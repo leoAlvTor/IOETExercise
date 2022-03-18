5861b2c3ede719b7fdf34b54980572a7671dc8be
Leo Alvarado <calvaradot1@est.ups.edu.ec>
2022-03-18 14:01:42 -0500

    - New private method: __get_next_day. Allows getting next day in special cases. - New public method: get_output. Generates a string containing the salary to be paid. - New static method: open_file. Allows reading and a new file and storing its lines as a list.
    - Method calculate_salary modified: Special case when the working hours start from day A and ends on the next day.
    
    - Method calculate_hour_price modified: Special case when the end hour is equal to zero becomes 24.
    
    - Method get_worked_days modified: Using regular expressions to check string integrity.

--- payment.py
+++ payment.py
@@ -1,4 +1,6 @@
+import os.path
 from datetime import datetime as dt
+import re
 
 
 class Payment:
@@ -20,10 +22,11 @@ class Payment:
         '18:01-00:00': 20,
     }
 
-    weekend_bonus: int = 5
+    WEEKEND_BONUS: int = 5
 
     def __init__(self, user_input):
         self.__init_week_time_hours_dict()
+        self.regular_expression = re.compile('[A-Z]+[0-9]+[0-9]+:[0-9]+-[0-9]+:[0-9]+')
         self.user_input = user_input
 
     def __init_week_time_hours_dict(self) -> None:
@@ -35,34 +38,57 @@ class Payment:
     def get_worked_days(self) -> list:
         split_input = self.user_input.split('=')
         self.user_name = split_input[0]
-        worked_days: list = split_input[1].split(',')
+        worked_days: list = self.regular_expression.findall(split_input[1])
         return worked_days
 
     def split_time(self, hours: list) -> tuple:
         return dt.strptime(hours[0], '%H:%M').hour, dt.strptime(hours[1], '%H:%M').hour
 
-    def calculate_hour_price(self, day: str, hours: list) -> float:
-        start_time, end_time = self.split_time(hours)
+    def calculate_hour_price(self, day: str, start_time, end_time) -> float:
         price = 0
         for key, time_tuple in self.week_time_hours.items():
             value = list(time_tuple)
             value[1] = 24 if value[1] == 0 else value[1]
+            end_time = 24 if end_time == 0 else end_time
             if start_time >= value[0] and end_time <= value[1]:
                 week_payment = self.week_time_payment.get(key) if day in self.WEEK_DAYS else self.week_time_payment.get(
-                    key) + 5
+                    key) + self.WEEKEND_BONUS
                 return abs(end_time - start_time) * week_payment
             elif start_time >= value[0] and end_time > value[1]:
                 price = abs(value[1] - start_time) * self.week_time_payment.get(key)
                 continue
             elif start_time < value[0] and end_time <= value[1]:
                 week_payment = self.week_time_payment.get(key) if day in self.WEEK_DAYS else self.week_time_payment.get(
-                    key) + 5
+                    key) + self.WEEKEND_BONUS
                 return abs(end_time - value[0]) * week_payment + price
 
+    def __get_next_day(self, current_day: str):
+        if current_day == 'FR':
+            return 'SA'
+        elif current_day == 'SU':
+            return 'MO'
+        else:
+            return self.WEEK_DAYS[self.WEEK_DAYS.index(current_day)+1]
+
     def calculate_salary(self, worked_days: list) -> float:
-        salary = 0
+        salary: float = 0
         for element in worked_days:
-            day: str = element[0:2]
-            hours: list = element[2:].split('-')
-            salary += self.calculate_hour_price(day, hours)
+            day, hours = element[0:2], element[2:].split('-')
+            start_hour, end_hour = self.split_time(hours)
+            if start_hour > end_hour:
+                salary += self.calculate_hour_price(day, start_hour, 0)
+                salary += self.calculate_hour_price(self.__get_next_day(day), 0, end_hour)
+                continue
+            salary += self.calculate_hour_price(day, start_hour, end_hour)
         return salary
+
+    def get_output(self, salary: float) -> str:
+        return f'The amount to pay {self.user_name} is: {salary} USD'
+
+    @staticmethod
+    def open_file(file_name: str = 'ACME payment.txt') -> list:
+        if not os.path.exists(file_name):
+            raise 'error: File not found!'
+        with open(file_name, 'r') as file:
+            file_text: list = ''.join(file.readlines()).split('\n')
+        return file_text
