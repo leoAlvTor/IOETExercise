fb2a0558df96929d25327970f1b29ce3cf58e36a
Leo Alvarado <calvaradot1@est.ups.edu.ec>
2022-03-20 00:05:55 -0500

    Main calls command line utility for communicating with end user.

--- main.py
+++ main.py
@@ -1,19 +1,5 @@
-import sys
-import payment
+from utilities import command_line_utility
 
 if __name__ == '__main__':
-    if len(sys.argv) < 1:
-        raise 'Error, no input received'
-    else:
-        paymentList = payment.Payment.open_file(sys.argv[1])
-
-        for paymentString in paymentList:
-            paymentObject = payment.Payment(paymentString)
-            worked_days = paymentObject.get_worked_days()
-            if len(worked_days) > 0:
-                salary = paymentObject.calculate_salary(worked_days)
-                output = paymentObject.get_output(salary)
-                print(output)
-            else:
-                raise Exception('Payment string does not match pattern XX00:00-00:00 where XX represents the first '
-                                'two letters of week day.')
+    print(command_line_utility.get_welcome_str())
+    command_line_utility.choose_option()
